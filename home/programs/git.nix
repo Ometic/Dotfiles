2a56d7ff7fdad56e117371ee6dd3e2b753381130
Ometic <iam.ometic@pm.me>
2023-09-14 17:26:56 +0700

    feat(programs/git): commit sign using 1password
    quick config need change soon

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -6,5 +6,20 @@
 
     userEmail = "iam.ometic@pm.me";
     userName = "Ometic";
+
+    extraConfig = ''
+    [user]
+  signingkey = ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMtp59EptaeN8WrOZy4W7KtW6YFNyvVJXsih8sFllUu2
+
+[gpg]
+  format = ssh
+
+[gpg "ssh"]
+  program = "/etc/profiles/per-user/ometic/bin/op-ssh-sign"
+
+[commit]
+  gpgsign = true
+
+    '';
   };
 }
