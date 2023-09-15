d3863b27f406d20512d4a78157b143d22fc47ba7
Ometic <iam.ometic@pm.me>
2023-09-15 19:59:02 +0700

    feat(home/programs): add xdg

--- home/profiles/zelix.nix
+++ home/profiles/zelix.nix
@@ -4,6 +4,7 @@
 
     ../desktop/hyprland
 
+    ../programs/xdg.nix
     ../programs/git.nix
     ../programs/ssh.nix
     ../programs/foot.nix
---
+++ home/programs/xdg.nix
@@ -0,0 +1,16 @@
+{
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      documents = "$HOME/Documents";
+      download = "$HOME/Downloads";
+      videos = "$HOME/Media";
+      pictures = "$HOME/Media";
+      music = "$HOME/Music";
+      desktop = "$HOME/Other";
+      publicShare = "$HOME/Other";
+      templates = "$HOME/Other";
+    }; 
+  };  
+}
